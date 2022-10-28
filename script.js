c7f5744fb571b7853f3de4df9e0d83f83f9d9491
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-10-28 16:43:08 +0200

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+let names = ["Rachel", "Ross", "Monica", "Phoebe", "Chandler", "Joey", "Gunther", "Smelly Cat", "Naked Neighbor"]
+
+addNames = () => {
+    let name = document.querySelector("p")
+}
